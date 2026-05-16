86d19a24b6a560f51aa73a8b6f7adab832dcd0d0
Abdelrhman-Hany <bodyhany457@gmail.com>
2026-05-16 12:30:49 +0300

    Edit file names

--- os/Main.java
+++ os/Main.java
@@ -5,9 +5,6 @@ import javax.swing.*;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
          SwingUtilities.invokeLater(() -> {
             SchedulerGUI gui = new SchedulerGUI();
